01acf9c9855bebbcf3a9f170f683ac7b95451cd9
ziyad <ziyadjamalali@gmail.com>
2021-08-29 14:25:20 +0700

    packege

---
+++ Belajar/Belajar..kt
@@ -0,0 +1,18 @@
+package Belajar
+
+class teacher(val name:String){
+    public fun ziyad(){
+
+        println("zyad")
+    }
+}
+
+fun main(){
+
+var ziya = teacher("ziyad")
+println(ziya.name)
+println(ziya.ziyad())
+}
+
+
+
